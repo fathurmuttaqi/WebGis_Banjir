6db09bc66c45c22da7468188a3dc59bf71ee7264
fathurmuttaqi <fathurmuttaqi1202@gmail.com>
2026-07-20 00:26:24 +0700

    Fix auth routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,45 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const db = require("../database"); // sesuaikan dengan file koneksi MySQL kamu
+const {
+  login,
+  register,
+} = require("../controllers/authController");
 
-router.post("/login", (req, res) => {
+// Register
+router.post("/register", register);
 
-    const { email, password } = req.body;
-
-    const sql = `
-        SELECT *
-        FROM users
-        WHERE email = ? AND password = ?
-    `;
-
-    db.query(sql, [email, password], (err, result) => {
-
-        if (err) {
-            return res.status(500).json({
-                success: false,
-                message: "Database Error"
-            });
-        }
-
-        if (result.length === 0) {
-
-            return res.json({
-                success: false,
-                message: "Email atau Password salah"
-            });
-
-        }
-
-        return res.json({
-
-            success: true,
-            user: result[0]
-
-        });
-
-    });
-
-});
+// Login
+router.post("/login", login);
 
 module.exports = router;
