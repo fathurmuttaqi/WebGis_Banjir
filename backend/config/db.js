82504cf52d133446571e8825ab18ab3dc6292be3
fathurmuttaqi <fathurmuttaqi1202@gmail.com>
2026-07-20 06:07:40 +0700

    fix railway mysql connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,31 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
+
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
+
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+
 });
 
-db.connect((err) => {
-    if (err) {
-        console.log(err);
-        return;
+
+db.getConnection((err, connection)=>{
+
+    if(err){
+        console.log("❌ MySQL Error:", err.message);
+    } else {
+        console.log("✅ MySQL Connected");
+        connection.release();
     }
 
-    console.log("✅ MySQL Connected");
 });
+
 
 module.exports = db;
